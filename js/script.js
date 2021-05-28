ec9f2c1cf91856a69a2082737a917b41c2b01b92
reallybadprogrammer <subintheplayer@gmail.com>
2021-05-28 12:17:40 +0900

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,424 @@
+const STORE = {
+ questions: {
+  accessibility: [
+   {
+    question: "What are some causes of air pollution in south korea?",
+    code: ``,
+    answers: [
+     "Vehicle Exhaust Fumes - Carbon Monoxide",
+     "Household Activities - Carbon Monoxide, Gas Appliances, Boilers, and Domestic Fires",
+     "Gas/Smoke that comes out of factories - Nitrogen, sulfur, and sometimes carbon",
+     "All of the Answers"
+    ],
+    correctAnswer: "All of the Answers"
+   },{
+    question: "How bad is the air pollution in south korea compared to china?",
+    code: ``,
+    answers: [
+     "China is way better",
+     "South Korea is normal,while China is a bit too high",
+     "All of the Answers",
+     "China is a million times worse",
+    ],
+    correctAnswer: "South Korea is normal,while China is a bit too high"
+   },{
+    question: "What is 1 way to reduce Sulphur Dioxide",
+    code: ``,
+    answers: [
+     "Cook with less oil",
+     "Stop burning things",
+     "Use Electric Oil",
+     "Plant trees",
+    ],
+    correctAnswer: "Cook with less oil"
+   },{
+    question: "What happens when Sulphur Dioxide and Nitrogen Oxide meet?",
+    code: ``,
+    answers: [
+     "Acid rain forms",
+     "Everyone Dies",
+     "Fire",
+     "No Answers are correct",
+    ],
+    correctAnswer: "Acid rain forms"
+   },{
+    question: "What can you and your parents do to reduce acid rain?",
+    code: ``,
+    answers: [
+     "Use less cars to reduce Nitrogen Oxide, and use less oil to reduce Sulphur Dioxide",
+     "Add a lot of air purifiers to the house",
+     "None of the above",
+     "All of the answers"
+    ],
+    correctAnswer: "Use less cars to reduce Nitrogen Oxide, and use less oil to reduce Sulphur Dioxide"
+   }
+  ],
+  arrays: [
+   {
+    question: "What does Sulphur Dioxide do to our body?",
+    code: ``,
+    answers: [
+     "Harm our Eyes",
+     "Harm our lungs",
+     "Make us Cough",
+     "All of the Answers"
+    ],
+    correctAnswer: "All of the Answers"
+   },{
+    question: "What is the biggest problem that Air Pollution is causing, at the date of today?",
+    code: ``,
+    answers: [
+     "Global Warming",
+     "Making me Cough",
+     "Making my eye sight bad",
+     "Not being able to see the stars"
+    ],
+    correctAnswer: "Global Warming"
+   },{
+    question: "What is the air particle that is affecting South Korea's air quality the most?",
+    code: ``,
+    answers: [
+     "Carbon Dioxide",
+     "Smoke",
+     "Sulphur Dioxide",
+     "Nitrogen Oxide"
+    ],
+    correctAnswer: "Sulphur Dioxide"
+   },{
+    question: "What country do you think South Korea is ranked for air pollution?",
+    code: ``,
+    answers: [
+     "1st",
+     "11th",
+     "53rd",
+     "None of the Answers"
+    ],
+    correctAnswer: "None of the Answers"
+   },{
+     question: "What do you think the Average AQI of Seoul in 2020 was?",
+     code: ``,
+     answers: [
+      "78",
+      "69",
+      "63",
+      "74"
+     ],
+    correctAnswer: "78"
+   }
+  ]
+ }
+};
+
+
+// Make the quiz. Create a model for our app's state
+function makeQuiz(){
+	// Creating an object to store the app's state when beginning the quiz
+	return {
+		// Gathering a random question out of the available questions for each category
+		questions: helpers.getRandomQuestions(STORE),
+		// Boolean for if the quiz is in progress or not
+		midQuiz: false,
+		// Array of correct/incorrect answers to use for our progress bar
+		progress: {
+			progressBar: [],
+			incorrectCategories: []
+		},
+		// Boolean to determine if the end state should display
+		completed: false,
+		// Monitoring which question we are currently on
+		currentQuestion: 0,
+		// Keeps track of total correct answers
+		correctAnswers: 0,
+		// Keeps current selected answer
+		currentAnswer: "",
+		// Keeps track of % completed
+		percCorrect: 0
+	}
+}
+
+// Apply fadeOut animations and set the stage for DOM text/element changes
+function $fade(appState){
+
+	// This is the completed state of of a quiz
+	if(appState.completed){
+
+		// Fade out elements with a promise to avoid choppy behavior
+		$.when($('.question-answer-wrapper, .question-wrapper, .answer-wrapper').fadeOut(500))
+			.done(function(){
+
+				// Display results of the quiz
+				$showResults(appState);
+				$('.results-wrapper').hide().removeClass('hide');
+				$('.question-answer-wrapper, .results-wrapper').fadeIn(500);
+	    });
+
+	// This is if the app is just starting
+	} else if(appState.midQuiz === false){
+
+		// Set a flag that the app has begun
+		appState.midQuiz = true;
+		// Fade out elements with a promise to avoid choppy behavior
+		$.when($('.question-answer-wrapper, .question-wrapper, .code, .answer-wrapper, .start-quiz, .quit-quiz, .results-wrapper, .progress, .progress-bar').fadeOut(500))
+			.done(function(){
+				// Remove any progress from a previous quiz (if any);
+				helpers.updateProgressBar(appState);
+				$('.progress-count').html('1 / 2');
+				$('.progress-perc').html('');
+				// Kill previous results
+				$('.failures').remove();
+				// Lots to do... mostly just setting up a new environment for a new quiz
+				$updateQuestion(appState);
+				$('progress-bar').empty();
+				$('progress-fill').html('Progress: <span class="progress-count">1 / 2</span><span class="progress-perc"></span>');
+				$('.question-answer-wrapper, .answer-wrapper').removeClass('begin');
+				$('.submit-btn, .progress, .progress-bar').removeClass('hide');
+				$('.progress, .progress-bar').hide();
+				$('.question-answer-wrapper, .question-wrapper, .answer-wrapper, .progress, .progress-bar').fadeIn(500);
+				if(!$('.code').hasClass('hide')){$('.code').fadeIn(500)};
+	    });
+
+	// This is if the app is in the middle of execution
+	} else if(appState.midQuiz){
+		$.when($('.question-answer-wrapper, .question-wrapper, .answer-wrapper').fadeOut(500))
+			.done(function(){
+				$updateQuestion(appState);
+				$('.question-answer-wrapper, .question-wrapper, .answer-wrapper').fadeIn(500);
+	    });
+	}
+}
+
+// Work in progress...
+function $showResults(appState){
+	if(appState.correctAnswers === 10){
+		let endMsg = `You got ${appState.percCorrect}% correct!
+You are very clever!
+`;
+	} else {
+		$('.answer-btn').remove();
+		let endMsg = `You got ${appState.percCorrect}% correct!`
+		let endFeedback = ``;
+		if(appState.progress.incorrectCategories.length === 0){
+			endFeedback = `You got everything Correct! Great Job!`;
+		}
+
+		$('.quiz-end-score').html(endMsg);
+		let $failList = $('<ul class="failures"></ul>');
+		appState.progress.incorrectCategories.map((cat => {
+			$failList.append("<li class='category'>" + cat + "</li>");
+		}));
+		$('.quiz-end-feedback-p').html(endFeedback);
+		$('.results-wrapper').removeClass('hide').css('display', 'flex');
+		$('.question-answer-wrapper, .results-wrapper, .quiz-end-feedback, .quiz-end-score, .retry-btn').css('display', 'flex').fadeIn(500);
+	}
+
+}
+
+// Update the question, code, answers, buttons in the DOM while we're in a faded out state
+function $updateQuestion(appState){
+	$('.answer-btn').remove();
+
+	// Update the question and code text with the current question
+	$('.question').html(appState.questions[appState.currentQuestion].question);
+
+	// This is a hack to hide/show the code portion
+	// Only 1/3 of the questions contain code so we hide it if they aren't present
+	if(appState.questions[appState.currentQuestion].code == ``){
+		$('.code').addClass('hide');
+	} else {
+		$('.code').removeClass('hide');
+		$('.code').html(`<pre>${appState.questions[appState.currentQuestion].code}</pre>`);
+	}
+
+	// Change continue back to submit
+	$('.continue-btn')
+		.val("Submit")
+		.removeClass('continue-btn')
+		.addClass('submit-btn')
+		.prop('disabled', true);
+
+	// Add in available answers for the question
+	let $answers = [];
+
+	// Adding the answers to a temporary array
+	for(let i=0; i<appState.questions[appState.currentQuestion].answers.length; i++){
+
+		// Add current question answers to an array
+		let $answer = $('<button class="answer-btn" type="button"></button>');
+		$answer.html(appState.questions[appState.currentQuestion].answers[i]);
+		$answers.push($answer);
+	}
+
+	// Shuffle the answers
+	helpers.shuffleAnswers($answers)
+
+	// Push answers to the DOM
+	$answers.forEach((answer) => {
+		$('.answer-wrapper').prepend(answer);
+	});
+}
+
+// Simple class and enable/disable DOM selection when answer is selected
+function selectAnswer(answer){
+	$('.answer-btn').removeClass('selected');
+	answer.addClass('selected');
+	$('.submit-btn').prop('disabled', false);
+}
+
+// Answer is selected and submitted
+// Push a feedback state
+function submitAnswer(appState){
+
+	// This will be returned true or false based on their answer
+	let correct;
+
+	// Add styles to the answers to show if their answer was correct or not
+	$('.answer-btn').each(function () {
+		if($(this).html() === appState.questions[appState.currentQuestion].correctAnswer){
+			$(this).addClass('pass');
+			// If answer is correct and selected...
+			if($(this).hasClass('selected')){
+				correct = "pass";
+				appState.correctAnswers++;
+				appState.questions[appState.currentQuestion];
+			}
+
+		// Handle correct answer if selected answer is incorrect
+		} else if ($(this).hasClass('selected')){
+			$(this).addClass('fail dim-answer');
+			correct = "fail";
+			appState.progress.incorrectCategories.push(appState.questions[appState.currentQuestion].category);
+
+		// Dim the other answers to make the correct one more prevalent
+		} else {
+			$(this).addClass('dim-answer');
+		}
+	});
+
+	// Add a progress bar indicator to our appState object
+	appState.progress.progressBar.push(`<div class="progress-indicator ${correct}"></div>`);
+
+	// Update our percent correct (parse a float and set it to a fixed percentage)
+	appState.percCorrect = parseFloat(appState.correctAnswers / (appState.currentQuestion + 1) * 100).toFixed();
+
+	// Update our current question VS total quiz length
+	$('.progress-count').html(`
+		${appState.currentQuestion + 1} / 2
+	`);
+
+	// Update our current correct percentage
+	$('.progress-perc').html(`
+		 // ${(appState.percCorrect)}% Correct
+	`)
+
+	// Change submit back to continue
+	$('.submit-btn')
+		.val("Continue")
+		.removeClass('submit-btn')
+		.addClass('continue-btn')
+
+	// Disable selecting answers
+	$('.answer-btn').prop("disabled", true);
+
+	// Update our progress-bar DOM
+	helpers.updateProgressBar(appState);
+
+	// Continue to next question
+	appState.currentQuestion++;
+
+	// Verify if we're done or not
+	if(appState.currentQuestion === appState.questions.length){
+		appState.completed = true;
+	}
+
+}
+
+// Silly easter egg for saying you don't want to do the quiz
+function killQuiz(){
+	$('.start-quiz, .quit-quiz').hide();
+	let failureMsg = "You have to do it."
+	let msgSplit = failureMsg.split(" ");
+	let counter = 0;
+	$('.question').empty();
+	let startTroll = setInterval(function () {
+		$('.question').append(msgSplit[counter] + " ");
+		counter++;
+		if(counter > msgSplit.length - 1){
+  			clearInterval(startTroll);
+  			$('.start-quiz').text('You can do it! Start Quiz').fadeIn(500);
+		}
+	}, 250);
+}
+
+// These are simple algorithms to modify data that don't need an individual function
+let helpers = {
+	// Pick a random question from the available ones
+	pickRandomQ: function(obj,section){
+		return Math.floor(Math.random() * obj.questions[section].length);
+	},
+	// Gather a random question from every category
+	getRandomQuestions: function(obj){
+		let questions = [];
+		let categories = Object.keys(obj.questions);
+		categories.forEach((cat) => {
+			let randomQ = this.pickRandomQ(obj,cat);
+			let question = obj.questions[cat][randomQ];
+			question.category = cat;
+			questions.push(question);
+		});
+		return questions;
+	},
+	// Shuffle the answers so they don't appear in the same order
+	shuffleAnswers: function(arr){
+	    for (var i = arr.length - 1; i > 0; i--) {
+	        var j = Math.floor(Math.random() * (i + 1));
+	        var temp = arr[i];
+	        arr[i] = arr[j];
+	        arr[j] = temp;
+	    }
+	},
+	// Update progress bar DOM element
+	updateProgressBar: function(appState){
+		$('.progress-bar').empty();
+		appState.progress.progressBar.forEach((progInd => {
+			$('.progress-bar').append(progInd);
+		}))
+	}
+}
+
+// Lets start the show
+$(function(){
+
+	// Kill any form refresh
+	$('.answer-wrapper').on('submit', function(e){
+		e.preventDefault();
+	});
+
+	// Storage for quiz app state
+	let quizData;
+
+	// Start quiz
+	$('.start-quiz, .retry-btn').on('click', function(){
+		quizData = makeQuiz();
+		$fade(quizData);
+	});
+
+	// Select an answer
+	$('.question-answer-wrapper').on('click', '.answer-btn', function(){
+		selectAnswer($(this));
+	})
+
+	// Submit your answer to display feedback and advance question counter
+	$('.question-answer-wrapper').on('click', '.submit-btn', function(e){
+		submitAnswer(quizData);
+	});
+
+	// Submit your answer to display feedback and advance question counter
+	$('.question-answer-wrapper').on('click', '.continue-btn', function(){
+		$fade(quizData);
+	});
+
+	// Easter egg to chastize the user
+	$('.quit-quiz').on('click', function(){
+		killQuiz();
+	})
+})
